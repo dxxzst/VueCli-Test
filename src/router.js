fa890f58dd4d2b9f5f9bf623a96b72867ce35c5a
XueSong <song.xue@seek-data.com>
2018-11-22 09:35:59 +0800

    some test

--- src/router.js
+++ src/router.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Login from './views/login/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
+      path: '/login',
+      name: 'login',
+      component: Login, //() => import('./views/login/index'),
+      hidden: true
+    },
+    {
       path: '/',
       name: 'home',
       component: Home
