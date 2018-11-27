1b617317e8d005003054feb564cdd12587ce7d13
XueSong <song.xue@seek-data.com>
2018-11-27 17:04:42 +0800

    基本跑起来

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
 import ElementUI from 'element-ui'
 
-import router from './router'
-import store from './store'
+import App from './App'
+import router from './router/router'
+import store from './store/store'
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.scss' // global css
 
-Vue.use(ElementUI)
-Vue.config.productionTip = false
+import '@/icons' // icon
+import '@/permission' // permission control
+
+Vue.use(ElementUI);
 
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
