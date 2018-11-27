c6697eec3861c73573ea35331d3b0d65049e05f3
XueSong <song.xue@seek-data.com>
2018-11-27 18:34:44 +0800

    研究本地化

---
+++ alias.config.js
@@ -0,0 +1,14 @@
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+module.exports = {
+    resolve: {
+        alias: {
+            '@': resolve('src'),
+            'common': resolve('src/common'),
+            'api': resolve('src/api'),
+            'xxx': resolve('src/xxxx')
+        }
+    }
+};
--- src/permission.js
+++ src/permission.js
@@ -9,16 +9,14 @@ NProgress.configure({
     showSpinner: false
 }); // NProgress Configuration
 
-const whiteList = ['/login'] // no redirect whitelist
+const whiteList = ['/login']; // no redirect whitelist
 
 router.beforeEach((to, from, next) => {
-    NProgress.start() // start progress bar
+    NProgress.start(); // start progress bar
     if (getToken()) { // determine if there has token
         /* has token*/
         if (to.path === '/login') {
-            next({
-                path: '/'
-            })
+            next({path: '/'});
             NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
         } else {
             if (store.getters.roles.length === 0) { // 判断当前用户是否已拉取完user_info信息
@@ -28,35 +26,33 @@ router.beforeEach((to, from, next) => {
                     store.dispatch('GenerateRoutes', {
                         roles
                     }).then(() => { // 根据roles权限生成可访问的路由表
-                        router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
+                        router.addRoutes(store.getters.addRouters);// 动态添加可访问路由表
                         next({
                             ...to,
                             replace: true
-                        }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
-                    })
+                        }); // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
+                    });
                 }).catch((err) => {
                     store.dispatch('FedLogOut').then(() => {
-                        Message.error(err || 'Verification failed, please login again')
-                        next({
-                            path: '/'
-                        })
-                    })
-                })
+                        Message.error(err || 'Verification failed, please login again');
+                        next({path: '/'});
+                    });
+                });
             } else {
-                next()
+                next();
             }
         }
     } else {
         /* has no token*/
         if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
-            next()
+            next();
         } else {
-            next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
-            NProgress.done()
+            next(`/login?redirect=${to.path}`); // 否则全部重定向到登录页
+            NProgress.done();
         }
     }
-})
+});
 
 router.afterEach(() => {
     NProgress.done() // finish progress bar
-})
+});
