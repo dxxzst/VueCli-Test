702d2553f9ee599aeb3447ee01db160b5ef68c3f
XueSong <song.xue@seek-data.com>
2018-11-28 10:50:21 +0800

    一些格式化

--- src/utils/request.js
+++ src/utils/request.js
@@ -19,7 +19,7 @@ service.interceptors.request.use(
     },
     error => {
         // Do something with request error
-        console.log(error) // for debug
+        console.log(error); // for debug
         Promise.reject(error);
     }
 );
@@ -30,13 +30,13 @@ service.interceptors.response.use(
         /**
          * code为非20000是抛错 可结合自己业务进行修改
          */
-        const res = response.data
+        const res = response.data;
         if (res.code !== 20000) {
             Message({
                 message: res.message,
                 type: 'error',
                 duration: 5 * 1000
-            })
+            });
 
             // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
             if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
@@ -51,8 +51,8 @@ service.interceptors.response.use(
                 ).then(() => {
                     store.dispatch('FedLogOut').then(() => {
                         location.reload() // 为了重新实例化vue-router对象 避免bug
-                    })
-                })
+                    });
+                });
             }
             return Promise.reject('error')
         } else {
@@ -60,14 +60,14 @@ service.interceptors.response.use(
         }
     },
     error => {
-        console.log('err' + error) // for debug
+        console.log('err' + error); // for debug
         Message({
             message: error.message,
             type: 'error',
             duration: 5 * 1000
-        })
+        });
         return Promise.reject(error)
     }
-)
+);
 
-export default service
+export default service;
