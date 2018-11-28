1ae11f28fb12effdf7c8d3682772e571fa867860
XueSong <song.xue@seek-data.com>
2018-11-28 13:45:03 +0800

    各种代码格式化

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,24 +5,22 @@ import {getToken} from '@/utils/auth';
 
 // 创建axios实例
 const service = axios.create({
-    baseURL: process.env.BASE_API, // api 的 base_url
+    baseURL: process.env.VUE_APP_BASE_API, // api 的 base_url
     timeout: 15000 // 请求超时时间
 });
 
 // request拦截器
-service.interceptors.request.use(
-    config => {
+service.interceptors.request.use(config => {
         if (store.getters.token) {
-            config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+            config.headers['AUTHORITY'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
         }
-        return config
+        return config;
     },
     error => {
         // Do something with request error
         console.log(error); // for debug
         Promise.reject(error);
-    }
-);
+    });
 
 // response 拦截器
 service.interceptors.response.use(
