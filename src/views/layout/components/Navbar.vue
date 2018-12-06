2aac15e2d3424e29bc57c1ee5c2d57ac6f775507
XueSong <song.xue@seek-data.com>
2018-12-06 11:01:20 +0800

    update

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
     <el-menu class="navbar" mode="horizontal">
-        <hamburger :toggle-click="toggleSideBar" :is-active="sidebar.opened" class="hamburger-container"/>
+        <hamburger :is-active="sidebar.opened" :toggle-click="toggleSideBar" class="hamburger-container"/>
         <breadcrumb/>
         <el-dropdown class="avatar-container" trigger="click">
             <div class="avatar-wrapper">
-                <img :src="avatar ? avatar : defaultAvatar + '?imageView2/1/w/80/h/80'" class="user-avatar">
+                <img :src="avatar ? avatar : defaultAvatar + '?imageView2/1/w/80/h/80'" class="user-avatar" alt="头像">
                 <i class="el-icon-caret-bottom"></i>
             </div>
-            <el-dropdown-menu slot="dropdown" class="user-dropdown">
+            <el-dropdown-menu class="user-dropdown" slot="dropdown">
                 <router-link class="inlineBlock" to="/">
                     <el-dropdown-item>Home</el-dropdown-item>
                 </router-link>
                 <el-dropdown-item divided>
-                    <span style="display:block;" @click="logout">LogOut</span>
+                    <span @click="logout" style="display:block;">LogOut</span>
                 </el-dropdown-item>
             </el-dropdown-menu>
         </el-dropdown>
@@ -59,32 +59,38 @@
         height: 50px;
         line-height: 50px;
         border-radius: 0 !important;
+
         .hamburger-container {
             line-height: 58px;
             height: 50px;
             float: left;
             padding: 0 10px;
         }
+
         .screenfull {
             position: absolute;
             right: 90px;
             top: 16px;
             color: red;
         }
+
         .avatar-container {
             height: 50px;
             display: inline-block;
             position: absolute;
             right: 35px;
+
             .avatar-wrapper {
                 cursor: pointer;
                 margin-top: 5px;
                 position: relative;
+
                 .user-avatar {
                     width: 40px;
                     height: 40px;
                     border-radius: 10px;
                 }
+
                 .el-icon-caret-bottom {
                     position: absolute;
                     right: -20px;
