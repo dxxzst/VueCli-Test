650bcf1871a198eb908ab87bd8750ec7075af71f
XueSong <song.xue@seek-data.com>
2018-12-06 11:21:10 +0800

    some changes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-
 import App from './App';
 import router from './router/router';
 import store from './store/store';
 import 'normalize.css/normalize.css'; // A modern alternative to CSS resets
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/styles/index.scss'; // global css
-
 import '@/icons/index'; // icon
 import '@/permission'; // permission control
 
