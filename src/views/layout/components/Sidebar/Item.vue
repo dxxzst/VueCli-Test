0a4a4768d2fa584d125e02ed8e63c101baefff85
XueSong <song.xue@seek-data.com>
2018-12-07 15:51:07 +0800

    item 优化

--- src/views/layout/components/Sidebar/Item.vue
+++ src/views/layout/components/Sidebar/Item.vue
@@ -1,29 +1,41 @@
 <script>
-export default {
-  name: 'MenuItem',
-  functional: true,
-  props: {
-    icon: {
-      type: String,
-      default: ''
-    },
-    title: {
-      type: String,
-      default: ''
-    }
-  },
-  render(h, context) {
-    const { icon, title } = context.props
-    const vnodes = []
+    export default {
+        name: 'MenuItem',
+        functional: true,
+        props: {
+            icon: {
+                type: String,
+                default: ''
+            },
+            title: {
+                type: String,
+                default: ''
+            }
+        },
+        render(createElement, context) {
+            const {icon, title} = context.props;
+            const vNodes = [];
 
-    if (icon) {
-      vnodes.push(<svg-icon icon-class={icon}/>)
+            if (icon) {
+                //vNodes.push(<svg-icon icon-class={icon}/>);
+                vNodes.push(createElement('svg-icon', {
+                    attrs: {
+                        'icon-class': icon
+                    },
+                }));
+            }
+            if (title) {
+                //vNodes.push(<span slot='title'>{(title)}</span>);
+                vNodes.push(createElement('span', {
+                    attrs: {
+                        'slot': title
+                    },
+                    domProps: {
+                        innerHTML: title
+                    }
+                }));
+            }
+            return vNodes;
+        }
     }
-
-    if (title) {
-      vnodes.push(<span slot='title'>{(title)}</span>)
-    }
-    return vnodes
-  }
-}
 </script>
